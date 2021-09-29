779cd54d8317228416671f93d7673b9bbeef5f71
Kelvin Pires <pireskelvin7@gmail.com>
2021-09-29 15:55:59 -0300

    :sparkles: Create and add DOM variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const baseInput = document.querySelector("#baseInput");
+const baseSelect = document.querySelector("#baseSelect");
+
+const toCurrencyInput = document.querySelector("#toCurrencyInput");
+const toCurrencySelect = document.querySelector("#toCurrencySelect");
